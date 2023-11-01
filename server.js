31167adf9c93cba595dd068397f05727e5138a53
mahat sumit <mahatsumit5@gmail.com>
2023-11-02 01:55:29 +1100

    sss

--- server.js
+++ server.js
@@ -1,50 +1,3 @@
-// import dotenv from "dotenv";
-// dotenv.config();
-// import express from "express";
-// const app = express();
-// import cors from "cors";
-// const PORT = process.env.PORT || 8000;
-// import path from "path";
-// const _dirname = path.resolve();
-
-// // connect mongodb
-// import { mongoose } from "mongoose";
-
-// // middlewares
-// app.use(express.json());
-// app.use(
-//   cors({
-//     // origin: "http://localhost:3000", This alllows to set the request coming from the frontend
-//   })
-// );
-// app.use(express.static(_dirname + "/build"));
-
-// // API endpoints
-// import taskRouter from "./src/taskRouter/taskRouter.js";
-// app.use("/api/v1/task", taskRouter);
-
-// app.use("/", (req, res) => {
-//   res.sendFile(_dirname + "/index.html");
-// });
-
-// const dbLink =
-//   process.env.NODE_ENV !== "production"
-//     ? "mongodb://localhost:27017/nottododb"
-//     : process.env.MONGO_CLIENT;
-// mongoose
-//   .connect(dbLink)
-//   .then(() => {
-//     console.log("mongo conneted");
-//     app.listen(PORT, (err) => {
-//       err
-//         ? console.log(err.message)
-//         : console.log(`Server running at http://localhost:${PORT}`);
-//     });
-//   })
-//   .catch((error) => {
-//     console.log(error.message);
-//   });
-
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
@@ -53,12 +6,12 @@ import cors from "cors";
 const PORT = process.env.PORT || 8000;
 import path from "path";
 
-const __dirname = path.resolve();
+const _dirname = path.resolve();
 
 // middlewares
 app.use(express.json());
 app.use(cors());
-app.use(express.static(__dirname + "/build"));
+app.use(express.static(_dirname + "/build"));
 
 // API endpoints
 import taskRouter from "./src/taskRouter/taskRouter.js";
@@ -67,14 +20,14 @@ import mongoose from "mongoose";
 app.use("/api/v1/task", taskRouter);
 
 app.use("/", (req, res) => {
-  res.sendFile(__dirname + "/build/index.html");
+  res.sendFile(_dirname + "/build/index.html");
 });
-// console.log(__dirname);
 
-////////
-
-const dbLink = process.env.MONGO_CLIENT;
-
+const dbLink =
+  process.env.NODE_ENV === "production"
+    ? process.env.MONGO_CLIENT
+    : "mongodb://127.0.0.1:27017/nottododb";
+console.log(dbLink);
 mongoose
   .connect(dbLink)
   .then(() => {
@@ -88,4 +41,7 @@ mongoose
   .catch((error) => {
     console.log(error.message);
   });
+app.listen(PORT, (REQ, RES) => {
+  console.log("sERVER IS UP AND RUNNING");
+});
 // open port for http request to access the server
